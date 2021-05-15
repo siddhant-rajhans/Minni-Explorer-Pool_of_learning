de0a32450e08e81977a94c8d9cf99a9dbff806eb
vaibhav saini <vbsaini2004@gmail.com>
2021-05-15 16:50:43 +0530

    navbar done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,30 @@
 // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+    scrollFunction();
+};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "3px 1px";
-    document.getElementById("logo").style.fontSize = "15px";
-  } else {
-    document.getElementById("navbar").style.padding = "8px 1px";
-    document.getElementById("logo").style.fontSize = "25px";
-  }
+    if (window.scrollY > 80) {
+        document.getElementById("ham").style.marginTop = "42.5px";
+        document.querySelector(".logo").classList.add("logosmall");
+        document.getElementById("navbar").classList.add("smallheight");
+        document.getElementById("navbar").classList.add("smallmine");
+    } else if (window.scrollY == 0) {
+        document.querySelector(".logo").classList.remove("logosmall");
+        document.getElementById("ham").style.marginTop = "62.5px";
+        document.getElementById("navbar").classList.remove("smallmine");
+        document.getElementById("navbar").classList.remove("smallheight");
+    }
+}
+window.onload = function () {
+    if (window.innerWidth <= 877) {
+        document.getElementById("ham").classList.remove("hide");
+    } else {
+        document.getElementById("navbar-right").classList.remove("hide");
+    }
+};
+function Hambar(ham) {
+    ham.classList.toggle("change");
+    document.getElementById("navbar-right").classList.toggle("hide");
+    document.getElementById("navbar").classList.toggle("height");
 }
